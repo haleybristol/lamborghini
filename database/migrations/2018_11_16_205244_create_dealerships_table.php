75d89ff36e86056ba552d52be3853fa6050fe8ab
Jeremy Kenedy <jeremykenedy@gmail.com>
2018-11-16 16:42:46 -0800

    debug

--- database/migrations/2018_11_16_205244_create_dealerships_table.php
+++ database/migrations/2018_11_16_205244_create_dealerships_table.php
@@ -15,14 +15,11 @@ class CreateDealershipsTable extends Migration
     {
         Schema::create('dealerships', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('store_name', 255)->unique();
             $table->string('contact_name', 255)->default('Marketing Manager');
-            $table->string('email', 191);
             $table->string('language', 255)->default('English');
             $table->string('phone', 100)->nullable();
             $table->string('address', 255)->nullable();
             $table->timestamps();
-            $table->unique([DB::raw('email(191)')]);
         });
     }
 
