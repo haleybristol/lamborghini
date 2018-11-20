ff2b02e03f999f7267a0af1e1c57e4ae42f34604
Haley Bristol <hebristol@gmail.com>
2018-11-20 15:07:59 -0800

    style select dropdown

--- public/js/app.js
+++ public/js/app.js
@@ -13888,6 +13888,7 @@ module.exports = __webpack_require__(43);
 /* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -13908,8 +13909,253 @@ window.Vue = __webpack_require__(36);
 Vue.component('example-component', __webpack_require__(39));
 
 var app = new Vue({
-  el: '#app'
+	el: '#app'
 });
+
+////////////////////////////////////
+// prerequisite utility functions //
+// the real stuff starts below    //
+////////////////////////////////////
+var util = {
+	f: {
+		addStyle: function addStyle(elem, prop, val, vendors) {
+			var i, ii, property, value;
+			if (!util.f.isElem(elem)) {
+				elem = document.getElementById(elem);
+			}
+			if (!util.f.isArray(prop)) {
+				prop = [prop];
+				val = [val];
+			}
+			for (i = 0; i < prop.length; i += 1) {
+				var thisProp = String(prop[i]),
+				    thisVal = String(val[i]);
+				if (typeof vendors !== "undefined") {
+					if (!util.f.isArray(vendors)) {
+						vendors.toLowerCase() == "all" ? vendors = ["webkit", "moz", "ms", "o"] : vendors = [vendors];
+					}
+					for (ii = 0; ii < vendors.length; ii += 1) {
+						elem.style[vendors[i] + thisProp] = thisVal;
+					}
+				}
+				thisProp = thisProp.charAt(0).toLowerCase() + thisProp.slice(1);
+				elem.style[thisProp] = thisVal;
+			}
+		},
+		cssLoaded: function cssLoaded(event) {
+			var child = util.f.getTrg(event);
+			child.setAttribute("media", "all");
+		},
+		events: {
+			cancel: function cancel(event) {
+				util.f.events.prevent(event);
+				util.f.events.stop(event);
+			},
+			prevent: function prevent(event) {
+				event = event || window.event;
+				event.preventDefault();
+			},
+			stop: function stop(event) {
+				event = event || window.event;
+				event.stopPropagation();
+			}
+		},
+		getSize: function getSize(elem, prop) {
+			return parseInt(elem.getBoundingClientRect()[prop], 10);
+		},
+		getTrg: function getTrg(event) {
+			event = event || window.event;
+			if (event.srcElement) {
+				return event.srcElement;
+			} else {
+				return event.target;
+			}
+		},
+		isElem: function isElem(elem) {
+			return util.f.isNode(elem) && elem.nodeType == 1;
+		},
+		isArray: function isArray(v) {
+			return v.constructor === Array;
+		},
+		isNode: function isNode(elem) {
+			return (typeof Node === 'undefined' ? 'undefined' : _typeof(Node)) === "object" ? elem instanceof Node : elem && (typeof elem === 'undefined' ? 'undefined' : _typeof(elem)) === "object" && typeof elem.nodeType === "number" && typeof elem.nodeName === "string" && elem.nodeType !== 3;
+		},
+		isObj: function isObj(v) {
+			return (typeof v === 'undefined' ? 'undefined' : _typeof(v)) == "object";
+		},
+		replaceAt: function replaceAt(str, index, char) {
+			return str.substr(0, index) + char + str.substr(index + char.length);
+		}
+	}
+},
+
+//////////////////////////////////////
+// ok that's all the utilities      //
+// onto the select box / form stuff //
+//////////////////////////////////////
+form = {
+	f: {
+		init: {
+			register: function register() {
+				var child,
+				    children = document.getElementsByClassName("field"),
+				    i;
+				for (i = 0; i < children.length; i += 1) {
+					child = children[i];
+					util.f.addStyle(child, "Opacity", 1);
+				}
+				children = document.getElementsByClassName("psuedo_select");
+				for (i = 0; i < children.length; i += 1) {
+					child = children[i];
+					child.addEventListener("click", form.f.select.toggle);
+				}
+			},
+			unregister: function unregister() {
+				//just here as a formallity
+				//call this to stop all ongoing timeouts are ready the page for some sort of json re-route
+			}
+		},
+		select: {
+			blur: function blur(field) {
+				field.classList.remove("focused");
+				var child,
+				    children = field.childNodes,
+				    i,
+				    ii,
+				    nested_child,
+				    nested_children;
+				for (i = 0; i < children.length; i += 1) {
+					child = children[i];
+					if (util.f.isElem(child)) {
+						if (child.classList.contains("deselect")) {
+							child.parentNode.removeChild(child);
+						} else if (child.tagName == "SPAN") {
+							if (!field.dataset.value) {
+								util.f.addStyle(child, ["FontSize", "Top"], ["16px", "32px"]);
+							}
+						} else if (child.classList.contains("psuedo_select")) {
+							nested_children = child.childNodes;
+							for (ii = 0; ii < nested_children.length; ii += 1) {
+								nested_child = nested_children[ii];
+								if (util.f.isElem(nested_child)) {
+									if (nested_child.tagName == "SPAN") {
+										if (!field.dataset.value) {
+											util.f.addStyle(nested_child, ["Opacity", "Transform"], [0, "translateY(24px)"]);
+										}
+									} else if (nested_child.tagName == "UL") {
+										util.f.addStyle(nested_child, ["Height", "Opacity"], [0, 0]);
+									}
+								}
+							}
+						}
+					}
+				}
+			},
+			focus: function focus(field) {
+				field.classList.add("focused");
+				var bool = false,
+				    child,
+				    children = field.childNodes,
+				    i,
+				    ii,
+				    iii,
+				    nested_child,
+				    nested_children,
+				    nested_nested_child,
+				    nested_nested_children,
+				    size = 0;
+				for (i = 0; i < children.length; i += 1) {
+					child = children[i];
+					util.f.isElem(child) && child.classList.contains("deselect") ? bool = true : null;
+				}
+				if (!bool) {
+					child = document.createElement("div");
+					child.className = "deselect";
+					child.addEventListener("click", form.f.select.toggle);
+					field.insertBefore(child, children[0]);
+				}
+				for (i = 0; i < children.length; i += 1) {
+					child = children[i];
+					if (util.f.isElem(child) && child.classList.contains("psuedo_select")) {
+						nested_children = child.childNodes;
+						for (ii = 0; ii < nested_children.length; ii += 1) {
+							nested_child = nested_children[ii];
+							if (util.f.isElem(nested_child) && nested_child.tagName == "UL") {
+								size = 0;
+								nested_nested_children = nested_child.childNodes;
+								for (iii = 0; iii < nested_nested_children.length; iii += 1) {
+									nested_nested_child = nested_nested_children[iii];
+									if (util.f.isElem(nested_nested_child) && nested_nested_child.tagName == "LI") {
+										size += util.f.getSize(nested_nested_child, "height");
+										console.log("size: " + size);
+									}
+								}
+								util.f.addStyle(nested_child, ["Height", "Opacity", "Max-height", "Overflow"], [size + "px", 1, "300px", 'auto']);
+							}
+						}
+					}
+				}
+			},
+			selection: function selection(child, parent) {
+				var children = parent.childNodes,
+				    i,
+				    ii,
+				    nested_child,
+				    nested_children,
+				    time = 0,
+				    value;
+				if (util.f.isElem(child) && util.f.isElem(parent)) {
+					parent.dataset.value = child.dataset.value;
+					value = child.innerHTML;
+				}
+				for (i = 0; i < children.length; i += 1) {
+					child = children[i];
+					if (util.f.isElem(child)) {
+						if (child.classList.contains("psuedo_select")) {
+							nested_children = child.childNodes;
+							for (ii = 0; ii < nested_children.length; ii += 1) {
+								nested_child = nested_children[ii];
+								if (util.f.isElem(nested_child) && nested_child.classList.contains("selected")) {
+									if (nested_child.innerHTML) {
+										time = 1E2;
+										util.f.addStyle(nested_child, ["Opacity", "Transform"], [0, "translateY(24px)"], "all");
+									}
+									setTimeout(function (c, v) {
+										c.innerHTML = v;
+										util.f.addStyle(c, ["Opacity", "Transform", "TransitionDuration"], [1, "translateY(0px)", ".1s"], "all");
+									}, time, nested_child, value);
+								}
+							}
+						} else if (child.tagName == "SPAN") {
+							util.f.addStyle(child, "Opacity", "0");
+						}
+					}
+				}
+			},
+			toggle: function toggle(event) {
+				util.f.events.stop(event);
+				var child = util.f.getTrg(event),
+				    children,
+				    i,
+				    parent;
+				switch (true) {
+					case child.classList.contains("psuedo_select"):
+					case child.classList.contains("deselect"):
+						parent = child.parentNode;
+						break;
+					case child.classList.contains("options"):
+						parent = child.parentNode.parentNode;
+						break;
+					case child.classList.contains("option"):
+						parent = child.parentNode.parentNode.parentNode;
+						form.f.select.selection(child, parent);
+						break;
+				}
+				parent.classList.contains("focused") ? form.f.select.blur(parent) : form.f.select.focus(parent);
+			}
+		}
+	} };
+window.onload = form.f.init.register;
 
 /***/ }),
 /* 13 */
@@ -47414,7 +47660,7 @@ if (false) {
 /* 43 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \nimage[Attributes Style] {\n     ^\n      Invalid operator in attribute selector for Attributes\n      in /Users/haley.bristol/sites/lambourghini/resources/sass/_landing.scss (line 213, column 7)\n    at runLoaders (/Users/haley.bristol/sites/lambourghini/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/haley.bristol/sites/lambourghini/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/haley.bristol/sites/lambourghini/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/haley.bristol/sites/lambourghini/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/haley.bristol/sites/lambourghini/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Users/haley.bristol/sites/lambourghini/node_modules/neo-async/async.js:8077:18)\n    at options.error (/Users/haley.bristol/sites/lambourghini/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
 
             <main>
                 @yield('content')
+                
             </main>
 
             @include('partials.footer')
