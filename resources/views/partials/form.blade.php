5c751aed577c6892ac2ace61602691c3d4df310a
Haley Bristol <hebristol@gmail.com>
2018-11-28 15:57:02 -0800

    add frontend form validation

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -4,25 +4,25 @@
 	<div class="dot-container">
 		<div class="row">
 			<div class="col-12 col-sm-6">
-				<span class="circle first"></span><input class="{{ $errors->has('firstname') ? 'required' : '' }}" type="text" name="firstname" placeholder="{!! trans('form.placeholder.firstname') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.firstname') !!}'">
+				<span class="circle first"></span><input class="{{ $errors->has('firstname') ? 'required' : '' }}" type="text" name="firstname" placeholder="{!! trans('form.placeholder.firstname') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.firstname') !!}'" required>
 			</div>
 			<div class="col-12 col-sm-6">
-				<span class="circle last"></span><input class="{{ $errors->has('lastname') ? 'required' : '' }}" type="text" name="lastname" placeholder="{!! trans('form.placeholder.lastname') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.lastname') !!}'">
+				<span class="circle last"></span><input class="{{ $errors->has('lastname') ? 'required' : '' }}" type="text" name="lastname" placeholder="{!! trans('form.placeholder.lastname') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.lastname') !!}'" required>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-12 col-sm-6">
-				<span class="circle email"></span><input class="{{ $errors->has('email') ? 'required' : '' }}" type="text" name="email" placeholder="{!! trans('form.placeholder.email') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.email') !!}'">
+				<span class="circle email"></span><input class="{{ $errors->has('email') ? 'required' : '' }}" type="text" name="email" placeholder="{!! trans('form.placeholder.email') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.email') !!}'" required>
 			</div>
 			<div class="col-12 col-sm-6">
-				<span class="circle phone"></span><input class="{{ $errors->has('phone') ? 'required' : '' }}" type="number" name="phone" placeholder="{!! trans('form.placeholder.number') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.number') !!}'">
+				<span class="circle phone"></span><input class="{{ $errors->has('phone') ? 'required' : '' }}" type="number" name="phone" placeholder="{!! trans('form.placeholder.number') !!}" onfocus="this.placeholder = ''" onblur="this.placeholder = '{!! trans('form.placeholder.number') !!}'" required>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-12 col-sm-6">
 				<span class="circle country"></span>
-				<div class="custom-select" id="country" class="psuedo_select">
-					<select name="country">
+				<div class="custom-select {{ $errors->has('country') ? 'required' : '' }}" id="country" class="psuedo_select">
+					<select name="country" required>
 						<option value="">Country or Region</option>
 						<option>Afghanistan</option>
 						<option>Albania</option>
@@ -278,7 +278,7 @@
 				<span class="circle"></span>
 				<div id="dealership" class="custom-select">
 					<select name="dealer" required>
-						<option value="">Preferred Dealership</option>
+						<option value="" class="{{ $errors->has('dealer') ? 'required' : '' }}">Preferred Dealership</option>
 						@foreach ($dealerShips as $dealership)
 							<option value="{{ $dealership->store_name }}" data-contact-name="{{ $dealership->contact_name }}" data-email="{{ $dealership->email }}" data-phone="{{ $dealership->phone }}">
 							{{ $dealership->store_name }}
@@ -290,7 +290,7 @@
 		</div>
 	</div>
 	<div class="check-send">
-		<div class="form-group">
+		<div class="check-box">
 			<input type="checkbox" id="policy" name="checkbox" required>
 			<label for="policy">View our <a href="https://www.lamborghini.com/en-en/privacy-legal" target="_blank"><u>privacy policy</u></a>. I agree to the processing of my personal data for marketing purposes.</label>
 		</div>
