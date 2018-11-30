8ea25005858037c3a0370ccbe00f0c2befc4a4cb
Haley Bristol <hebristol@gmail.com>
2018-11-30 10:03:57 -0800

    edit header trans() to work better with arabic

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -9,8 +9,7 @@ return [
     |
     */
     'block1' => [
-        'header'        => 'SUV FUNCTIONALITY.',
-        'headerSpan'    => 'SUPER SPORTS CAR SOUL.',
+        'header'        => '<b>SUV FUNCTIONALITY.</b> <span>SUPER SPORTS CAR SOUL.</span>',
         'line1'         => 'The Urus: a pure Lamborghini in its own right.',
         'line2'         => 'You can see that unmistakable DNA in the extreme proportions. The distinctive silhouette with dynamic flying coupé line. And, of course, the outstanding performance and driving dynamics. All of which culminates in the world’s first Super Sports Utility Vehicle.',
         'line3'         => 'Start your journey now. Leave your details below to have our concierge set an appointment.'
--- resources/lang/fr/app.php
+++ resources/lang/fr/app.php
@@ -10,8 +10,7 @@ return [
     */
     
     'block1' => [
-        'header'        => 'FONCTIONNALITÉ SUV.',
-        'headerSpan'    => 'âme de voiture de sport super.',
+        'header'        => '<b>FONCTIONNALITÉ SUV.</b> <span>âme de voiture de sport super.</span>',
         'line1'         => "L'Urus: une Lamborghini pure à part entière.",
         'line2'         => 'Vous pouvez voir cet ADN indubitable dans les proportions extrêmes. La silhouette distinctive avec la ligne de coupé volante dynamique. Et, bien sûr, la performance exceptionnelle et la dynamique de conduite. Le tout culmine avec le premier véhicule utilitaire Super Sport au monde.',
         'line3'         => 'Commencez votre voyage maintenant. Laissez vos coordonnées ci-dessous pour que notre concierge fixe un rendez-vous.'
