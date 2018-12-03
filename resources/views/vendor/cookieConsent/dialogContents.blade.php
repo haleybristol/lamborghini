f000af9fb23384d0c6d7586a8e0aa6231337ea89
Haley Bristol <hebristol@gmail.com>
2018-12-03 12:00:46 -0800

    cookie debugging and edit bottom slice

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -7,5 +7,7 @@
     <button class="js-cookie-consent-agree cookie-consent__agree">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
-
+    <!-- <button type="button" class="close text-right" data-dismiss="modal" aria-label="Close">
+        <span class="icon-close">&#10005;</span>
+    </button> -->
 </div>
