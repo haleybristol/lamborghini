a8a9f1240ae3cabcbc81a2b7875123cf069e6db4
Haley Bristol <hebristol@gmail.com>
2018-12-03 13:51:32 -0800

    edit x style for exit cookie pop up

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -2,13 +2,15 @@
 
     <p class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
+        
+        <button onclick="hideCookieDialog()" type="button" class="close cookie text-right" aria-label="Close">
+            <span class="icon-close">&#10005;</span>
+        </button>
     </p>
 
     <button class="js-cookie-consent-agree cookie-consent__agree">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
 
-    <button type="button" class="close cookie text-right" aria-label="Close">
-        <span class="icon-close">&#10005;</span>
-    </button>
+
 </div>
--- resources/views/vendor/cookieConsent/index.blade.php
+++ resources/views/vendor/cookieConsent/index.blade.php
@@ -47,14 +47,10 @@
             };
         })();
 
-            function hideCookieDialog() {
-                const dialogs = document.getElementsByClassName('js-cookie-consent');
-
-                for (let i = 0; i < dialogs.length; ++i) {
-                    dialogs[i].style.display = 'none';
-                }
-            }
-            $('.cookie').click(hideCookieDialog);
+        function hideCookieDialog() {
+            const dialogs = document.getElementsByClassName('js-cookie-consent');
+            dialogs.style.display = 'none';
+        }
     </script>
 
 @endif
