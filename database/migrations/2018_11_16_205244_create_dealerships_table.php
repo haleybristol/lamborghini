3904cccbb522256b19f9c3cd57586a2a1ad8ca09
Haley Bristol <hebristol@gmail.com>
2018-12-03 17:01:05 -0800

    add all dealerships to seeder

--- database/migrations/2018_11_16_205244_create_dealerships_table.php
+++ database/migrations/2018_11_16_205244_create_dealerships_table.php
@@ -18,7 +18,6 @@ class CreateDealershipsTable extends Migration
             $table->string('store_name', 255);
             $table->string('contact_name', 255)->default('Marketing Manager');
             $table->string('email', 191);
-            $table->string('language', 255)->default('English');
             $table->string('country', 255)->nullable();
             $table->string('address', 255)->nullable();
             $table->string('website', 255)->default('https://www.lamborghini.com/en-en/');
