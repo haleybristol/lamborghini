7ffdc714d133f8c9c39cec2a1d5f8064cfd56939
Haley Bristol <hebristol@gmail.com>
2018-12-08 01:01:30 -0800

    add arabic and cookie disclaimer

--- resources/lang/vendor/cookieConsent/de/texts.php
+++ resources/lang/vendor/cookieConsent/de/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => "Diese Seite verwendet Cookies um das Nutzererlebnis zu steigern. <a href='https://www.lamborghini.com/de-en/cookie-policy' target='_blank'>cookies</a>.",
+    'message' => "Diese Website setzt technische Cookies sowie Drittanbieter-Cookies zur Erstellung von Surfprofilen ein, um Werbebotschaften entsprechend den Online-Navigationspräferenzen des Nutzers zu übermitteln. Indem Sie fortfahren, erklären Sie Ihr Einverständnis mit dem Einsatz dieser <a href='https://www.lamborghini.com/de-en/cookie-policy' target='_blank'>cookies</a>.",
     'agree' => 'Akzeptieren',
 ];
--- resources/lang/vendor/cookieConsent/fr/texts.php
+++ resources/lang/vendor/cookieConsent/fr/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => "Ce site nécessite l'autorisation de cookies pour fonctionner correctement <a href='https://www.lamborghini.com/fr-en/cookie-policy' target='_blank'>cookies</a>.",
+    'message' => "Ce site Web utilise des techniques et des cookies de profilage de tiers pour envoyer des messages publicitaires en ligne correspondant aux préférences de navigation de l’utilisateur en ligne. En poursuivant, vous consentez à l'utilisation de ces <a href='https://www.lamborghini.com/fr-en/cookie-policy' target='_blank'>cookies</a>.",
     'agree' => 'Accepter',
 ];
