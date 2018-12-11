b7fe4f271ddbd6e5b417dadcaeb785f29c01c74a
Haley Bristol <hebristol@gmail.com>
2018-12-11 11:04:55 -0800

    add styling for outlook email

--- resources/views/emails/thankyou.blade.php
+++ resources/views/emails/thankyou.blade.php
@@ -1,48 +1,59 @@
+<style>
+  body {
+   margin:0;
+   padding:0;
+  }
+</style>
 <div style="background-color: #333333; width: 100%;color: white;height: 100px;">
     <div style="text-align:center;">
         <img style="height: 80px; width: 80px; padding-top: 10px;" src="https://www.lamborghini.com/themes/custom/lambo/img/base/logo.png">
     </div>
 </div>
-<div align="center" style="margin-top: 60px;">
-    <div align="left" style="width: 520px; margin: 0 auto;">
-        <h1 >{!! trans('email.hi') !!} {{ $contactName }}, </h1>
-        <p style="font-size: 18px;">{!! trans('email.copy') !!}</p>
-    </div>
+<div style="margin-top: 60px;">
+    <table align="center" style="width: 520px;">
+        <tr><td><h1 >{!! trans('email.hi') !!} {{ $contactName }}, </h1></td></tr>
+        <tr><td><p style="font-size: 18px;">{!! trans('email.copy') !!}</p></td></tr>
+    </table>
     <div align="center" style="width: 520px; margin: 0 auto;">
         <table style="border-collapse: collapse; width: 520px;">
             <tr>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">First Name</td>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $firstname }}</td>   
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">First Name</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">{{ $firstname }}</td>   
             </tr>
             <tr style="background-color: #dddddd; background: #dddddd;">
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Last Name</td> 
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $lastname }}</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">Last Name</td> 
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">{{ $lastname }}</td>
             </tr>
             <tr>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Email</td>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $email }}</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">Email</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">{{ $email }}</td>
             </tr>
             <tr style="background-color: #dddddd; background: #dddddd;">
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Phone Number</td>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $phone }}</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">Phone Number</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">{{ $phone }}</td>
             </tr>
             <tr>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Country/Region</td>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $country }}</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">Country/Region</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">{{ $country }}</td>
             </tr>
             <tr style="background-color: #dddddd; background: #dddddd;">
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Dealership</td>
-                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $dealer }}</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">Dealership</td>
+                <td style="border:  1px solid #B1B1B1; text-align: left; padding: 8px;">{{ $dealer }}</td>
             </tr>
         </table>
     </div>
-    <div style="text-align: center; margin: 50px 0;">
-        <a style="background: #e0b401; color: white; text-decoration: none; text-transform: uppercase; padding: 10px 20px;" href="mailto:{{ $email }}">Respond to lead</a>
-    </div>
+    <div style="margin-bottom: 30px">&nbsp;</div>
+    <table align="center" cellpadding="12" width="220px">
+        <tr style="background: #e0b401; width: 100%; text-align: center;">
+            <td><a style="text-decoration: none; color: white; text-transform: uppercase;" href="mailto:{{ $email }}">Respond to lead</a></td>
+        </tr>
+    </table>
+    <div style="margin-bottom: 60px">&nbsp;</div>
 </div>
-<div style="background-color: #333333;width: 100%;color: white; padding: 10px; height: 100px;">
-  <div style="margin: 0 auto; width: 520px; font-size: 14px">
-      <p>Copyright ©2018 Automobili Lamborghini S.p.A. a sole shareholder company part of Audi Group. All rights reserved. VAT no. IT 00591801204</p>
-      <a style="color: white; text-decoration: underline;" href="https://www.lamborghini.com/en-en/privacy-legal" target="_blank">Privacy Policy</a>
-  </div>
-</div>
+
+<table cellpadding="50" cellspacing="0" border="0" width="100%">
+    <tr style="background-color: #333333;width: 100%;color: white;">
+        <td>Copyright ©2018 Automobili Lamborghini S.p.A. a sole shareholder company part of Audi Group. All rights reserved. VAT no. IT 00591801204</td>
+        <td><a style="color: white; text-decoration: underline;" href="https://www.lamborghini.com/en-en/privacy-legal" target="_blank">Privacy Policy</a></td>
+    </tr>
+</table>
