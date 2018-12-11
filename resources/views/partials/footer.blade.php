f56201970b2bda363dc18c28e6f4b61853a43dfd
Haley Bristol <hebristol@gmail.com>
2018-12-10 16:54:21 -0800

    right adjust footer for arabic

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,14 @@
-<footer>
+<footer class="{{Config::get('app.locale') == 'ar' ? '' : 'display-none'}}">
+  <div class="container row">
+    <div class="col-12 col-sm-3 ">
+      <a href="{!! trans('app.policy-link') !!}"  target="_blank">{!! trans('app.footer.policy') !!}</a>
+    </div>
+    <div class="col-12 col-sm-9 foot-right">
+      <p>{!! trans('app.footer.copyright') !!}</p>
+    </div>
+  </div>
+</footer>
+<footer class="{{Config::get('app.locale') == 'ar' ? 'display-none' : ''}}">
   <div class="container row">
     <div class="col-12 col-sm-9">
       <p>{!! trans('app.footer.copyright') !!}</p>
