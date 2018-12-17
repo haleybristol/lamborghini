63407a67db0c989453364072150df11982c5c9fb
Haley Bristol <hebristol@gmail.com>
2018-12-17 11:16:46 -0800

    add direction right to footer"

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="{{Config::get('app.locale') == 'ar' ? '' : 'display-none'}}">
   <div class="container row">
-    <div class="col-12 col-sm-3 align-right">
+    <div class="col-12 col-sm-3 dir-right">
       <a href="{!! trans('app.policy-link') !!}"  target="_blank">{!! trans('app.footer.policy') !!}</a>
     </div>
-    <div class="col-12 col-sm-9 foot-right">
+    <div class="col-12 col-sm-9 foot-right right-align">
       <p>{!! trans('app.footer.copyright') !!}</p>
     </div>
   </div>
