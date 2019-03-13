6b5e91549195d9cb384c814c0ddfffd1d645e440
Ry <ryan.back@sq1.com>
2019-03-13 08:43:27 -0700

    Open social links in new tab

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,13 +3,13 @@
         <div class="privacy-and-social">
             <div class="privacy-and-legal">
 
-                <a href="{!! trans('app.policy-link') !!}"  target="_blank">
+                <a href="{!! trans('app.policy-link') !!}" target="_blank">
                     <span>
                         {!! trans('app.footer.policy') !!}
                     </span>
                 </a>
 
-                <a href="{!! trans('app.policy-link') !!}"  target="_blank">
+                <a href="{!! trans('app.policy-link') !!}" target="_blank">
                     <span>
                         LAMBORGHINI.COM
                     </span>
@@ -17,34 +17,34 @@
             </div>
 
             <div class="social-icons">
-                <a href="//www.facebook.com/Lamborghini/">
+                <a href="//www.facebook.com/Lamborghini/" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Facbook Icon' class="icon icon-facebook"></span>
                 </a>
-                <a href="//twitter.com/lamborghini">
+                <a href="//twitter.com/lamborghini" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Twitter Icon' class="icon icon-twitter-bird"></span>
                 </a>
-                <a href="//www.youtube.com/c/lamborghini">
+                <a href="//www.youtube.com/c/lamborghini" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Youtube Icon' class="icon icon-youtube-play"></span>
                 </a>
-                <a href="//www.instagram.com/lamborghini/">
+                <a href="//www.instagram.com/lamborghini/" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Instagram Icon' class="icon icon-instagram"></span>
                 </a>
-                <a href="//www.linkedin.com/company/automobili-lamborghini-s-p-a-/">
+                <a href="//www.linkedin.com/company/automobili-lamborghini-s-p-a-/" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Linkedin Icon' class="icon icon-linkedin"></span>
                 </a>
-                <a href="//www.pinterest.com/lamborghini/">
+                <a href="//www.pinterest.com/lamborghini/" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Pinterest Icon' class="icon icon-pinterest"></span>
                 </a>
-                <a href="//plus.google.com/+Lamborghini">
+                <a href="//plus.google.com/+Lamborghini" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Google Plus Icon' class="icon icon-gplus"></span>
                 </a>
-                <a href="//www.weibo.com/lamborghinichina">
+                <a href="//www.weibo.com/lamborghinichina" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Weibo Icon' class="icon icon-weibo"></span>
                 </a>
-                <a href="//i.youku.com/i/UMjg0MDg1ODMy">
+                <a href="//i.youku.com/i/UMjg0MDg1ODMy" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Youku Icon' class="icon icon-play-circled"></span>
                 </a>
-                <a href="//www.lamborghini.com/en-en/wechat">
+                <a href="//www.lamborghini.com/en-en/wechat" target='_blank'>
                     <span aria-hidden='true' role='presentation' aria-label='Wechat Icon' class="icon icon-wechat"></span>
                 </a>
             </div>
